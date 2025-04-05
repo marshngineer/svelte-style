48a54c5dac3530fb1588abeb1ec37c7f0850acd1
Marshall D. Nguyen <marshdev.social@gmail.com>
2025-04-05 11:29:26 +0700

    update css variables, shadcn install

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-	import Button from '$lib/components/button.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<Button>Hello worlds</Button>
+<Button variant="default" class="rounded-full text-[15.75px]" size="lg">Apply Now</Button>
